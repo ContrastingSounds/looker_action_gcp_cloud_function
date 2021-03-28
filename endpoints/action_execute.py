660ab2dfdc0d703901146c6ebb10598cfa564afe
Jon Walls <8075115+ContrastingSounds@users.noreply.github.com>
2021-03-28 18:39:28 +0100

    working API call

--- endpoints/action_execute.py
+++ endpoints/action_execute.py
@@ -4,6 +4,10 @@ import looker_sdk
 def action_execute(request):
     payload = request.get_json()
     print('action_execute received request:', payload)
+
+    sdk = looker_sdk.init31()
+    me = sdk.me()
+    print('sdk.user()', me)
     
     return {
         'statusCode': 200,
