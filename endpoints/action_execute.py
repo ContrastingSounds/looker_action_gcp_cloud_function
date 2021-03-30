0f3642bb65616688e1a7f7a76bb348a6ea094675
Jon Walls <8075115+ContrastingSounds@users.noreply.github.com>
2021-03-30 14:41:48 +0100

    get modified query based on original explore

--- endpoints/action_execute.py
+++ endpoints/action_execute.py
@@ -1,13 +1,34 @@
 import json
 import looker_sdk
 
+
 def action_execute(request):
     payload = request.get_json()
-    print('action_execute received request:', payload)
+    print('action_execute payload:', payload)
+    keys = ['type', 'scheduled_plan', 'attachment', 'data', 'form_params']
+    for key in keys:
+        print(key, ':', payload[key])
+
+    original_query = payload['scheduled_plan']['query']
+    print('query details:', original_query)
 
     sdk = looker_sdk.init31()
-    me = sdk.me()
-    print('sdk.user()', me)
+
+    modified_query = {
+        "view": "order_items",
+        "fields": ["order_items.user_id"] + original_query['fields'],
+        "filters": {
+            "order_items.created_date": "2021/01/01 to 2021/03/30"
+        },
+        "sorts": [
+            "order_items.created_date desc"
+        ],
+        "limit": "10",
+        "model": "hns_core",
+    }
+    resp = sdk.run_inline_query("json", modified_query)
+    resp = json.loads(resp)
+    print('response to modified query:', resp)
     
     return {
         'statusCode': 200,
